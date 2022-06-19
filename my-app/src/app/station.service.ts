e9241a6f60678610f3f27b37e1930b4f5e0a45fc
AnoniemeBlobvis <92373002+AnoniemeBlobvis@users.noreply.github.com>
2022-06-19 20:06:44 +0200

    progress in live data service

--- my-app/src/app/station.service.ts
+++ my-app/src/app/station.service.ts
@@ -8,21 +8,36 @@ import {BehaviorSubject, Observable} from "rxjs";
 })
 export class StationService {
   stationdata?:Stationdata
+  stationdataSet?:Stationdata[]
 
   public stationdata$ = new BehaviorSubject<Stationdata|undefined>(this.stationdata)
+  public stationdataSet$ = new BehaviorSubject<Stationdata[]|undefined>(this.stationdataSet)
 
   constructor() {
 
   }
 
-  getWeatherdata(): Observable<Stationdata|undefined> {
+  getWeatherData(): Observable<Stationdata|undefined> {
     return this.stationdata$.asObservable()
   }
 
   setWeatherData(id:number): void {
     fetch("http://localhost:8000/live/" + id).then(res => res.json()).then(res => {
-      this.stationdata = {id:res.id, geolocation:res.geolocation, date:res.date, time:res.time, temperature:res.temperature, wind_speed:res.wind_speed, wind_direction:res.wind_direction, rain:res.rain}
+      console.log(res)
+      this.stationdata = {id:res.id, geolocation:res.geolocation, date:res.date, time:res.time, temperature:res.temperature, wind_speed:res['wind-speed'], wind_direction:res['wind-direction'], rain:res.rain}
     }).then(res => this.stationdata$.next(this.stationdata))
   }
+
+  getWeatherDataSet(): Observable<Stationdata[]|undefined> {
+    return this.stationdataSet$.asObservable()
+  }
+
+  setWeatherDataSet(id:number): void {
+    fetch("http://localhost:8000/live/" + id).then(res => res.json()).then(res => {
+      console.log(res)
+      this.stationdata = {id:res.id, geolocation:res.geolocation, date:res.date, time:res.time, temperature:res.temperature, wind_speed:res['wind-speed'], wind_direction:res['wind-direction'], rain:res.rain}
+    }).then(res => this.stationdata$.next(this.stationdata))
+  }
+
 
 }
--- my-app/src/app/station/station.component.ts
+++ my-app/src/app/station/station.component.ts
@@ -9,7 +9,10 @@ import { StationService } from "../station.service";
   styleUrls: ['./station.component.css']
 })
 export class StationComponent implements OnInit {
-  stationdata:Stationdata = {id:249169, geolocation:"1", date:"2022-06-19T00:00:00+00:00", time:"1970-01-01T12:24:23+00:00", temperature:"10.6", wind_speed:11.2, wind_direction:227, rain:0.07}
+  stationdata?:Stationdata
+  wind_speed:String = "loading.."
+  wind_direction:String = "loading.."
+  rain:String = "loading.."
   private stationService:StationService = new StationService()
 
   constructor() {
@@ -20,7 +23,10 @@ export class StationComponent implements OnInit {
     this.stationService.stationdata$.subscribe(data => {
       if (data != undefined) {
         console.log(data)
-        this.station*data = data
+        this.stationdata = data
+        this.wind_speed = ''+data.wind_speed
+        this.wind_direction = ''+data.wind_direction
+        this.rain = ''+data.rain
       } else {
         this.stationService.setWeatherData(1)
       }
