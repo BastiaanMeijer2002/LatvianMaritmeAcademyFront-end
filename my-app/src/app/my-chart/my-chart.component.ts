89b5539d05a639fae9cb9ae9910802384d4bfaa0
DavidFaruntsev <81746948+DavidFaruntsev@users.noreply.github.com>
2022-06-19 14:01:22 +0200

    Chart aangepast

--- my-app/src/app/my-chart/my-chart.component.ts
+++ my-app/src/app/my-chart/my-chart.component.ts
@@ -14,25 +14,25 @@ export class MyChartComponent implements OnInit {
     const myChart = new Chart("myChart", {
       type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: 'Current data',
+          data: [12, 19, 3, 5, 2, 3, 7],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'blue'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'red'
+          ],
+          borderWidth: 1
+        },{
+          label: 'Las week data',
+          data: [14, 15, 7, 2, 9, 4, 8],
+          backgroundColor: [
+            'red'
+          ],
+          borderColor: [
+            'blue'
           ],
           borderWidth: 1
         }]
