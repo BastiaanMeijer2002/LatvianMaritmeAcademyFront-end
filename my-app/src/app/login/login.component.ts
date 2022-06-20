c8e6a189304f79c1c5a858547456321266c48165
SophieB1999 <sophiebroersma@outlook.com>
2022-06-20 19:54:57 +0200

    added login function

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,6 +11,28 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const loginButton = document.querySelector('loginButton')
+
+    loginButton?.addEventListener("click", this.loginEvent)
+
   }
 
+  loginEvent() {
+    let data = new FormData(document.querySelector('form') as HTMLFormElement)
+    let dataSend = JSON.stringify(Object.fromEntries(data as any))
+
+    fetch('http://localhost:8000/api/login_check',
+      {method: 'POST',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: dataSend
+      })
+      .then(resp => resp.json())
+      .then(json => {
+        localStorage.setItem('jwt', json.token)
+        window.location.replace('index.html')
+      })
+  }
 }
