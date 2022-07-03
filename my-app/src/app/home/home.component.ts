5b6d41ca722eb9e43257b3a0da37732dec531736
AnoniemeBlobvis <92373002+AnoniemeBlobvis@users.noreply.github.com>
2022-07-03 19:13:29 +0200

    Updatet some text to fit the usecase of the website

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SecurityService} from "../security.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private securityService: SecurityService) { }
 
   ngOnInit(): void {
+  }
+
+  isAuthorized(){
+    return this.securityService.getAuthStatus();
   }
 
 }
