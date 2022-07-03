ae43a438b9669a57752a69e4ed81d80058ec2f8b
AnoniemeBlobvis <92373002+AnoniemeBlobvis@users.noreply.github.com>
2022-07-03 20:54:05 +0200

    Charts now work offline and have normal date strings for labels

--- my-app/src/app/my-chart/my-chart.component.ts
+++ my-app/src/app/my-chart/my-chart.component.ts
@@ -12,6 +12,7 @@ import {Stationdata, StationdataSimple} from "../interfaces";
 })
 export class MyChartComponent implements OnInit {
   id = ""
+  state = "live";
   stationdata?:Stationdata
   stationdataSet?:StationdataSimple[]
   labels:string[] = []
@@ -23,43 +24,55 @@ export class MyChartComponent implements OnInit {
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
+    this.state = this.route.snapshot.params['state'];
 
-    this.stationService.stationdata$.subscribe(data => {
-      if (data != undefined) {
-        console.log(data)
-        this.stationdata = data
-        // @ts-ignore
-        document.getElementById('arrowfront').style.transform = `rotate(${data.wind_direction+90}deg)`
-      } else {
-        this.stationService.setWeatherData(parseInt(this.id))
-      }
-    })
+    if (this.state == "stored") {
+      this.getStoredData()
+    } else {
+      //set compass data
+      this.stationService.stationdata$.subscribe(data => {
+        if (data != undefined) {
+          console.log(data)
+          this.stationdata = data
+          // @ts-ignore
+          document.getElementById('arrowfront').style.transform = `rotate(${data.wind_direction + 90}deg)`
+        } else {
+          this.stationService.setWeatherData(parseInt(this.id))
+        }
+      })
 
-    this.stationService.stationdataSet$.subscribe(data => {
-      if (data != undefined) {
-        this.stationdataSet = data
-        this.labels = []
+      //set chart data
+      this.stationService.stationdataSet$.subscribe(data => {
+        if (data != undefined) {
+          this.stationdataSet = data
+          this.labels = []
 
-        this.rainChartData = []
-        this.windChartData = []
-        this.stationdataSet.forEach(stationdata => {
-          this.labels.push(stationdata.date)
-          this.rainChartData.push(stationdata.rainfall)
-          this.windChartData.push(stationdata.wind_speed)
-        })
+          this.rainChartData = []
+          this.windChartData = []
+          this.stationdataSet.forEach(stationdata => {
+            this.labels.push(stationdata.date.slice(0, 10))
+            this.rainChartData.push(stationdata.rainfall)
+            this.windChartData.push(stationdata.wind_speed)
+          })
 
-        rainChart.data.labels = this.labels
-        rainChart.data.datasets[0].data = this.rainChartData
-        rainChart.update()
+          rainChart.data.labels = this.labels
+          rainChart.data.datasets[0].data = this.rainChartData
+          rainChart.update()
 
-        windChart.data.labels = this.labels
-        windChart.data.datasets[0].data = this.windChartData
-        windChart.update()
-      } else {
-        this.stationService.setWeatherDataSet(parseInt(this.id), 7)
-      }
-    })
+          windChart.data.labels = this.labels
+          windChart.data.datasets[0].data = this.windChartData
+          windChart.update()
 
+          localStorage.setItem('labels', JSON.stringify(this.labels))
+          localStorage.setItem('windChartData', JSON.stringify(this.windChartData))
+          localStorage.setItem('rainChartData', JSON.stringify(this.rainChartData))
+        } else {
+          this.stationService.setWeatherDataSet(parseInt(this.id), 7)
+        }
+      })
+    }
+
+    // create charts
     const rainChart = new Chart("myChart", {
       type: 'line',
       data: {
@@ -88,10 +101,10 @@ export class MyChartComponent implements OnInit {
     const windChart = new Chart("myChartBar", {
       type: 'bar',
       data: {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        labels: this.labels,
         datasets: [{
           label: 'Current data',
-          data: [3, 22, 13, 6, 3, 3, 9],
+          data: this.windChartData,
           backgroundColor: [
             'rgba(222, 104, 247, 0.2)'
           ],
@@ -111,4 +124,22 @@ export class MyChartComponent implements OnInit {
     });
   }
 
+  getStoredData() {
+    let stationdata = localStorage.getItem('stationdata')
+    if (stationdata) {
+      this.stationdata = JSON.parse(stationdata)
+    }
+    let labels = localStorage.getItem('labels')
+    if (labels) {
+      this.labels = JSON.parse(labels)
+    }
+    let rainChartData = localStorage.getItem('rainChartData')
+    if (rainChartData) {
+      this.rainChartData = JSON.parse(rainChartData)
+    }
+    let windChartData = localStorage.getItem('windChartData')
+    if (windChartData) {
+      this.windChartData = JSON.parse(windChartData)
+    }
+  }
 }
